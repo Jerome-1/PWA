fda88e6bf8f557a1339a79e74567cbaa3994e34a
Jerome <hamiltoj@roehampton.ac.uk>
2022-03-28 16:39:51 +0100

    Visual changes and others

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+
 const httpPort = 80;
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 });
+
 
 app.listen(httpPort, function () {
     console.log(`Listening on port ${httpPort}!`)
